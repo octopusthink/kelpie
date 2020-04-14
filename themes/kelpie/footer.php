faf637a1699b72b31824d82f7b265aff634528f4
sarah <sarah@triggersandsparks.com>
2020-04-14 15:03:33 +0100

    feat: Add social icons to modal menu and footer.

--- themes/kelpie/footer.php
+++ themes/kelpie/footer.php
@@ -14,6 +14,30 @@
 ?>
 
 <footer id="colophon" class="site-footer">
+
+	<?php if ( has_nav_menu( 'social' ) ) : ?>
+		<nav aria-label="<?php esc_attr_e( 'Social links', 'kelpie' ); ?>" class="footer-social-wrapper">
+			<ul class="social-menu footer-social reset-list-style social-icons fill-children-current-color">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'social',
+						'container'       => '',
+						'container_class' => '',
+						'items_wrap'      => '%3$s',
+						'menu_id'         => '',
+						'menu_class'      => '',
+						'depth'           => 1,
+						'link_before'     => '<span class="screen-reader-text">',
+						'link_after'      => '</span>',
+						'fallback_cb'     => '',
+					)
+				);
+				?>
+			</ul><!-- .footer-social -->
+		</nav><!-- .footer-social-wrapper -->
+	<?php endif; ?>
+
 	<div class="site-info">
 		<p class="footer-copyright">&copy;
 			<?php
--- themes/kelpie/inc/template-tags.php
+++ themes/kelpie/inc/template-tags.php
@@ -206,3 +206,28 @@ function kelpie_site_description( $echo = true ) {
 
 	echo $html; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
+
+/**
+ * Display SVG icons in social links menu.
+ *
+ * @param  string  $item_output The menu item output.
+ * @param  WP_Post $item        Menu item object.
+ * @param  int     $depth       Depth of the menu.
+ * @param  array   $args        wp_nav_menu() arguments.
+ * @return string  $item_output The menu item output with social icon.
+ */
+function kelpie_nav_menu_social_icons( $item_output, $item, $depth, $args ) {
+	// Change SVG icon inside social links menu if there is supported URL.
+	if ( 'social' === $args->theme_location ) {
+		$svg = kelpie_SVG_Icons::get_social_link_svg( $item->url );
+		if ( empty( $svg ) ) {
+			$svg = kelpie_get_theme_svg( 'link' );
+		}
+		$item_output = str_replace( $args->link_after, '</span>' . $svg, $item_output );
+	}
+
+	return $item_output;
+}
+
+add_filter( 'walker_nav_menu_start_el', 'kelpie_nav_menu_social_icons', 10, 4 );
+
