b48f58cdd214f85024365dba465e0473b614ba2e
sarah <sarah@triggersandsparks.com>
2020-04-14 23:17:08 +0100

    feat: Style comment wrapper.

--- themes/kelpie/inc/template-tags.php
+++ themes/kelpie/inc/template-tags.php
@@ -480,3 +480,48 @@ function kelpie_nav_menu_social_icons( $item_output, $item, $depth, $args ) {
 
 add_filter( 'walker_nav_menu_start_el', 'kelpie_nav_menu_social_icons', 10, 4 );
 
+/**
+ * Comments
+ */
+/**
+ * Check if the specified comment is written by the author of the post commented on.
+ *
+ * @param object $comment Comment data.
+ *
+ * @return bool
+ */
+function kelpie_is_comment_by_post_author( $comment = null ) {
+
+	if ( is_object( $comment ) && $comment->user_id > 0 ) {
+
+		$user = get_userdata( $comment->user_id );
+		$post = get_post( $comment->comment_post_ID );
+
+		if ( ! empty( $user ) && ! empty( $post ) ) {
+
+			return $comment->user_id === $post->post_author;
+
+		}
+	}
+	return false;
+
+}
+
+/**
+ * Filter comment reply link to not JS scroll.
+ * Filter the comment reply link to add a class indicating it should not use JS slow-scroll, as it
+ * makes it scroll to the wrong position on the page.
+ *
+ * @param string $link Link to the top of the page.
+ *
+ * @return string $link Link to the top of the page.
+ */
+function kelpie_filter_comment_reply_link( $link ) {
+
+	$link = str_replace( 'class=\'', 'class=\'do-not-scroll ', $link );
+	return $link;
+
+}
+
+add_filter( 'comment_reply_link', 'kelpie_filter_comment_reply_link' );
+
